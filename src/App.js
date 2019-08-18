18defc5e92f7c8b85b14d29751335c61b84a96f5
Mikhail Maslyuk <leakymee@bu.edu>
2019-08-17 22:30:30 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
+import 'normalize.css'
 import './App.css';
 
-function App() {
+const initialState = {
+  firstName: "First Name",
+  lastName: "Last Name"
+}
+
+const Form = () => {
+  const [inputState, setInputState] = useState(initialState);
+  const [windowWidth, setWidth] = useState(window.innerWidth);
+
+  const handleChange = (event)  => {
+    // LOL
+    const { 
+      target: { 
+        value,
+        dataset: { name } 
+      }
+    } = event;
+    const newState = Object.assign(
+      {},
+      {...inputState},
+      {[name]: value}
+    )
+    setInputState(newState)
+  }
+
+  const handleResize = () => setWidth(window.innerWidth)
+
+  useEffect(() => {
+    console.log(inputState)
+  })
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    // If an effect returns a function, react will call it on UnMount, 
+    // This is appropriate for ending subscriptions
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="testable-form">
+        <input type="text" data-name="firstName"
+          value={inputState.firstName}
+          onChange={handleChange}
+        />
+        <input type="text" data-name="lastName"
+          value={inputState.lastName}
+          onChange={handleChange}
+        />
+      </form>
+      <div>
+        {`Width: ${windowWidth}`}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Form;
